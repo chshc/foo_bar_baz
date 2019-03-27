db8b15c6d10ba6212f46224120fbfb8d6cfb8d5f
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2019-03-27 07:42:13 -0500

    Uploaded Jack's program

---
+++ jack2.py
@@ -0,0 +1,32 @@
+print("")
+print("It's a secret to everyone!")
+print("\033[1;37m     _______    \033[0 ")
+print("\033[1;37m    /       \   \033[0 ")
+print("\033[1;37m   / \033[1;36m  I  I\033[0  \033[1;37m  \ \033[0 ")
+print("\033[1;37m   I _/----\_I  \033[0 ")
+print("\033[1;31m  /\033[0m \033[1;37m\       /\033[0\033[1;31m \ \033[0m ")
+print("\033[1;31m  I\033[0m\033[1;37m  \_____/\033[0m\033[1;31m  I \033[0m ")
+print("\033[1;31m  I           I \033[0m ")
+print("\033[1;31m  I           I \033[0m ")
+print("\033[1;31m /_____________\ \033[0m ")
+print("")
+
+print("What do you want?")
+print("1: Get Sword!")
+print("2: Get 20 Rupee! ")
+print("3: Leave, The oldman is a PEDO!")
+
+x = input()
+                                                                # x = input() is setting the variable X to whatever the user inputs.
+                                                                # x == input() is checking the variable for its value.
+if int(x) == 1:                                                 # the line `if x == "1":` would also work here
+    print("[Old Man] You wanted the sword!?")
+    print("[Old Man] You're a vicious child!")
+    print("[Old Man] Begone!")
+elif int(x) == 2:
+    print("[Old Man] You want the money?") 
+    print("[Old Man] But thats for my meds!?")
+elif int(x) == 3:
+    print("[Old Man] You made the right choice, I am a pedo.")
+else:
+    print("[Old Man] Your lack of a choice allowed the Old man ")
